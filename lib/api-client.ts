5fb9ce5617085fbee1cc0d8ca6ad7c03479d71f0
Ihor <dwarionas@gmail.com>
2025-12-07 21:38:52 +0100

    backend connect url

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://druido-server.vercel.app/";
 
 export interface ApiError {
 	message: string;
